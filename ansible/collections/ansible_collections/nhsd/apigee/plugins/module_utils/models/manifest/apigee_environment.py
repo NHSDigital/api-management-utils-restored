318b481d4350c019c595ed6b65fd90c9fa0ae411
Alex Hawdon <alex.hawdon1@nhs.net>
2023-11-15 16:05:57 +0000

    APM-4807: Remove anything to do with the API Catalogue
    Remove anything to do with the API Catalogue as it's not used any more

--- ansible/collections/ansible_collections/nhsd/apigee/plugins/module_utils/models/manifest/apigee_environment.py
+++ ansible/collections/ansible_collections/nhsd/apigee/plugins/module_utils/models/manifest/apigee_environment.py
@@ -1,8 +1,5 @@
 import typing
 import pydantic
-from ansible_collections.nhsd.apigee.plugins.module_utils.models.apigee.apidoc import (
-    ApigeeApidoc,
-)
 from ansible_collections.nhsd.apigee.plugins.module_utils.models.apigee.spec import (
     ApigeeSpec,
 )
@@ -16,7 +13,6 @@ class ManifestApigeeEnvironment(pydantic.BaseModel):
     name: LITERAL_APIGEE_ENVIRONMENTS
     products: typing.List[ApigeeProduct] = []
     specs: typing.List[ApigeeSpec] = []
-    api_catalog: typing.List[ApigeeApidoc] = []
 
     @pydantic.validator("products", "specs")
     def names_unique(cls, values):
@@ -24,22 +20,6 @@ class ManifestApigeeEnvironment(pydantic.BaseModel):
         if len(names) != len(set(names)):
             raise ValueError("Names are not unique")
         return values
-
-    @pydantic.validator("api_catalog")
-    def catalog_references(cls, api_catalog, values):
-        spec_names = [spec.name for spec in values.get("specs", [])]
-        product_names = [product.name for product in values.get("products", [])]
-
-        for item in api_catalog:
-            if item.edgeAPIProductName not in product_names:
-                raise ValueError(
-                    f"edgeAPIProductName {item.edgeAPIProductName} not in list of products in this environment"
-                )
-            if item.specId and item.specId not in spec_names:
-                raise ValueError(
-                    f"specId {item.specId} not in list of specs in this environment"
-                )
-        return api_catalog
 
     @pydantic.validator("products", pre=True)
     def set_single_environment(cls, products, values):
