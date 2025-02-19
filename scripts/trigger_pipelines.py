d1de8af8f75ebcc04ee4e9f6d3d5dce16905e274
NautiyalVikas1 <157499123+NautiyalVikas1@users.noreply.github.com>
2025-02-19 09:49:41 +0000

    APM-2488-Updated the test cases-print access token

--- scripts/trigger_pipelines.py
+++ scripts/trigger_pipelines.py
@@ -15,9 +15,9 @@ class AzureDevOps:
         self.client_secret = os.environ["AZ_CLIENT_SECRET"]
         self.client_tenant = os.environ["AZ_CLIENT_TENANT"]
         self.access_token = self._get_access_token()
-        self.notify_commit_sha = os.environ["NOTIFY_COMMIT_SHA"]
-        self.utils_pr_number = os.environ["UTILS_PR_NUMBER"]
-        self.notify_github_repo = "NHSDigital/api-management-utils"
+        # self.notify_commit_sha = os.environ["NOTIFY_COMMIT_SHA"]
+        # self.utils_pr_number = os.environ["UTILS_PR_NUMBER"]
+        # self.notify_github_repo = "NHSDigital/api-management-utils"
         self.api_request_delay = 60
 
     @staticmethod
