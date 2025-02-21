092fb05ed636c0ca53171af4c39c3b0d560d1925
NautiyalVikas1 <157499123+NautiyalVikas1@users.noreply.github.com>
2025-02-21 13:11:45 +0000

    APM-2488-Updated the test cases-print access token-test

--- scripts/test_pull_request_deployments.py
+++ scripts/test_pull_request_deployments.py
@@ -5,21 +5,11 @@ from trigger_pipelines import AzureDevOps
 
 
 PULL_REQUEST_PIPELINES = {
-    "identity-service": {
-        "build": 27,
-        "pr": 54,
-        "branch": "refs/heads/master"
-    },
     "canary-api": {
         "build": 222,
         "pr": 223,
         "branch": "refs/heads/main"
-    },
-    "personal-demographics-service": {
-        "build": 140,
-        "pr": 144,
-        "branch": "refs/heads/master"
-    },
+    }
 }
 
 
